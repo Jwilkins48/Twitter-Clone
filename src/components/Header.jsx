2c8e696289e866e62f28ea684fed84a427d1ea44
Jenna <jenna48071@gmail.com>
2022-06-25 18:50:47 -0500

    added icons to nav list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import image from '../components/images/whiteLogo.png'
+import twitterHome from '../components/images/homeLogo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouseUser, faHashtag, faBell, faEnvelope, faBookmark, faRectangleList, faUser, faCommentDots } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
+    const homeIcon = <FontAwesomeIcon icon={faHouseUser} />
+    const exploreIcon = <FontAwesomeIcon icon={faHashtag} />
+    const notificationIcon = <FontAwesomeIcon icon={faBell} /> 
+    const messageIcon = <FontAwesomeIcon icon={faEnvelope} />
+    const bookmarkIcon = <FontAwesomeIcon icon={faBookmark} />
+    const listIcon = <FontAwesomeIcon icon={faRectangleList} /> 
+    const profileIcon = <FontAwesomeIcon icon={faUser} />
+    const moreIcon = <FontAwesomeIcon icon={faCommentDots}/>
   return (
     <div className='navBar'>
         <img className='twitterIcon' src={image} alt='twitter icon' width="29" height="26"></img>
-        <p className='navItem'>Home</p>
-        <p className='navItem'>Explore</p>
-        <p className='navItem'>Notifications</p>
-        <p className='navItem'>Messages</p>
-        <p className='navItem'>Bookmarks</p>
-        <p className='navItem'>Lists</p>
-        <p className='navItem'>Profile</p>
-        <p className='navItem'>More</p>
+        <p className='navItem'>{homeIcon} Home</p>
+        <p className='navItem'>{exploreIcon} Explore</p>
+        <p className='navItem'>{notificationIcon} Notifications</p>
+        <p className='navItem'>{messageIcon} Messages</p>
+        <p className='navItem'>{bookmarkIcon} Bookmarks</p>
+        <p className='navItem'>{listIcon} Lists</p>
+        <p className='navItem'>{profileIcon} Profile</p>
+        <p className='navItem'>{moreIcon} More</p> 
 
         <button className='tweetBtn'>Tweet</button>
     </div>
