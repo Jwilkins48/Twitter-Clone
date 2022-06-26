186d9443e682b65ccfd17b481cd577805c16373a
Jenna <jenna48071@gmail.com>
2022-06-25 21:59:54 -0500

    added label to home screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import ProfilePicture from '../components/images/kitty.webp'
 
 function Home() {
   return (
     <div className='homeContainer'>
         <div className='homeMiddleSection'>
-            <h1>hi</h1><br></br>
-            <h1>orld</h1>
+            <div className='whatsHappeningContainer'>
+              <h3 className='homeHeader'>Home</h3>
+              <form className='postForm'>
+                <img className='profilePicture' src={ProfilePicture} alt='Cat profile' width="53" height="47"></img>
+                <input className='postInput' type='text' name='tweet' placeholder="What's happening?" />
+              
+                {/* <button>Tweet</button> */}
+
+              </form>
+              <button className='homeTweetBtn'>Tweet</button>
+            </div>
         </div>
     </div>
   )
